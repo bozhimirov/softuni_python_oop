9e5b139b58d8c5f75845291b9d0edd5a9566100c
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-06-20 12:39:46 +0300

    solved

---
+++ Classes and Objects - Exercise/04. Pizza Delivery.py
@@ -0,0 +1,32 @@
+class PizzaDelivery:
+    def __init__(self, name, price, ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False
+
+    def add_extra(self, ingredient, quantity, price_per_quantity):
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
+        if ingredient in self.ingredients:
+            self.ingredients[ingredient] += quantity
+        else:
+            self.ingredients[ingredient] = quantity
+        self.price += quantity * price_per_quantity
+
+    def remove_ingredient(self, ingredient, quantity, price_per_quantity):
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        if ingredient not in self.ingredients:
+            return f'Wrong ingredient selected! We do not use {ingredient} in {self.name}!'
+        if quantity > self.ingredients[ingredient]:
+            return f'Please check again the desired quantity of {ingredient}!'
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
+
+    def make_order(self):
+        self.ordered = not self.ordered
+        ingredients_str = ', '.join([f'{key}: {value}' for key, value in self.ingredients.items()])
+        return f"You've ordered pizza {self.name} prepared with {ingredients_str} and the price will be {self.price}lv."
+
