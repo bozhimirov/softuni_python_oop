05415866b982d70e9d9623ab06865035c5d4927f
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-07-08 12:54:21 +0300

    solved

---
+++ Iterators and Generators - Lab/01. Custom Range.py
@@ -0,0 +1,16 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.next_value = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.next_value > self.end:
+            raise StopIteration
+
+        value_to_return = self.next_value
+        self.next_value += 1
+        return value_to_return
